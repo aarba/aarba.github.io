0bf54eecd2bc9b75b8a376e282f66dc317762ac5
samveltadevosyan <stadevosyan@servicetitan.com>
2023-04-24 20:20:59 +0400

    add products content

---
+++ src/components/modal/modal.js
@@ -0,0 +1,29 @@
+import React, {useEffect} from 'react';
+import './modal.css';
+
+function Modal(props) {
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'Escape') {
+                props.onClose();
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [props.onClose]);
+
+    return (
+        <div className="modal-wrapper">
+            <div className="modal-background" onClick={props.onClose}></div>
+            <div className="modal-content">
+                {props.children}
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- src/sections/services.js
+++ src/sections/services.js
@@ -1,11 +1,37 @@
 import { useTranslation } from 'react-i18next';
+import {useState} from "react";
+import Modal from "../components/modal/modal"
+import ProductContent from "../components/product-content/product-content"
+import {products} from "../constants/products";
 
 export default function Services(params) {
   const { t } = useTranslation();
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
+  const onClick = (id) => {
+    setShowModal(true);
+    setModalContent(products.get(id));
+    document.documentElement.style.overflow = 'hidden';
+    document.body.scroll = 'no';
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setModalContent(null);
+    document.documentElement.style.overflow = 'auto';
+    document.body.scroll = 'yes';
+  }
 
   return (
     <section id="services" className="services">
+      {showModal && (
+          <Modal onClose={handleCloseModal}>
+            <ProductContent data={modalContent} />
+          </Modal>
+      )}
+
+
       <div className="container">
 
         <div className="section-title">
@@ -14,7 +40,7 @@ export default function Services(params) {
         </div>
 
         <div className="row">
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div className="col-lg-4 col-md-6 d-flex align-items-stretch c-p" onClick={() => onClick(1)}>
             <div className="icon-box">
               <div className="icon"><i className="fas fa-heartbeat"></i></div>
               <h4><a href="">{t('services.implantation')}</a></h4>
@@ -22,7 +48,7 @@ export default function Services(params) {
             </div>
           </div>
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 c-p" onClick={() => onClick(2)}>
             <div className="icon-box">
               <div className="icon"><i className="fas fa-pills"></i></div>
               <h4><a href="">{t('services.surgery')}</a></h4>
@@ -30,7 +56,7 @@ export default function Services(params) {
             </div>
           </div>
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0 c-p" onClick={() => onClick(3)}>
             <div className="icon-box">
               <div className="icon"><i className="fas fa-hospital-user"></i></div>
               <h4><a href="">{t('services.cosmetic')}</a></h4>
@@ -38,37 +64,38 @@ export default function Services(params) {
             </div>
           </div>
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-            <div className="icon-box">
-              <div className="icon"><i className="fas fa-dna"></i></div>
-              <h4><a href="">{t('services.surgery')}</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-            </div>
-          </div>
+          {/*commented by sam*/}
+          {/*<div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">*/}
+          {/*  <div className="icon-box">*/}
+          {/*    <div className="icon"><i className="fas fa-dna"></i></div>*/}
+          {/*    <h4><a href="">{t('services.surgery')}</a></h4>*/}
+          {/*    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-            <div className="icon-box">
-              <div className="icon"><i className="fas fa-notes-medical"></i></div>
-              <h4><a href="">{t('services.therapy')}</a></h4>
-              <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
-            </div>
-          </div>
+          {/*<div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">*/}
+          {/*  <div className="icon-box">*/}
+          {/*    <div className="icon"><i className="fas fa-notes-medical"></i></div>*/}
+          {/*    <h4><a href="">{t('services.therapy')}</a></h4>*/}
+          {/*    <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-            <div className="icon-box">
-              <div className="icon"><i className="fas fa-notes-medical"></i></div>
-              <h4><a href="">{t('services.pediatric')}</a></h4>
-              <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
-            </div>
-          </div>
+          {/*<div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">*/}
+          {/*  <div className="icon-box">*/}
+          {/*    <div className="icon"><i className="fas fa-notes-medical"></i></div>*/}
+          {/*    <h4><a href="">{t('services.pediatric')}</a></h4>*/}
+          {/*    <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-            <div className="icon-box">
-              <div className="icon"><i className="fas fa-notes-medical"></i></div>
-              <h4><a href="">{t('services.orthodontics')}</a></h4>
-              <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
-            </div>
-          </div>
+          {/*<div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">*/}
+          {/*  <div className="icon-box">*/}
+          {/*    <div className="icon"><i className="fas fa-notes-medical"></i></div>*/}
+          {/*    <h4><a href="">{t('services.orthodontics')}</a></h4>*/}
+          {/*    <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
         </div>
       </div>
