4b0be53ffdfd36ad9c2650dbc8e8454f65cb14b7
samveltadevosyan <stadevosyan@servicetitan.com>
2023-04-24 16:41:06 +0400

    gago

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -5,7 +5,7 @@ export const Contacts = () => {
         <div className="section-title">
           <h2>Contact</h2>
           <p>
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
+            nor comment commodi suscipit. Necessitatibus eius consequatur ex
             aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
             quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
             fugiat sit in iste officiis commodi quidem hic quas.
