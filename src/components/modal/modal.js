0e5dbbab0f6d41041a75ca42b8adcba7c20e5243
samveltadevosyan <stadevosyan@servicetitan.com>
2023-05-07 00:01:08 +0400

    eslint fixes

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,29 +1,27 @@
-import React, {useEffect} from 'react';
-import './modal.css';
+import React, { useEffect } from "react";
+import "./modal.css";
 
 function Modal(props) {
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            if (event.key === 'Escape') {
-                props.onClose();
-            }
-        };
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        props.onClose();
+      }
+    };
 
-        window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown);
 
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [props.onClose]);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [props.onClose]);
 
-    return (
-        <div className="modal-wrapper">
-            <div className="modal-background" onClick={props.onClose}></div>
-            <div className="modal-content">
-                {props.children}
-            </div>
-        </div>
-    );
+  return (
+    <div className="modal-wrapper">
+      <div className="modal-background" onClick={props.onClose}></div>
+      <div className="modal-content">{props.children}</div>
+    </div>
+  );
 }
 
 export default Modal;
--- src/sections/services.js
+++ src/sections/services.js
@@ -1,8 +1,9 @@
-import { useTranslation } from 'react-i18next';
-import {useState} from "react";
-import Modal from "../components/modal/modal"
-import ProductContent from "../components/product-content/product-content"
-import {products} from "../constants/products";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import Modal from "../components/modal/modal";
+import ProductContent from "../components/product-content/product-content";
+import { products } from "../constants/products";
+import ModelBlock from "../components/model-block/model-block";
 
 export default function Services(params) {
   const { t } = useTranslation();
@@ -12,56 +13,63 @@ export default function Services(params) {
   const onClick = (id) => {
     setShowModal(true);
     setModalContent(products.get(id));
-    document.documentElement.style.overflow = 'hidden';
-    document.body.scroll = 'no';
-  }
+    document.documentElement.style.overflow = "hidden";
+    document.body.scroll = "no";
+  };
 
   const handleCloseModal = () => {
     setShowModal(false);
     setModalContent(null);
-    document.documentElement.style.overflow = 'auto';
-    document.body.scroll = 'yes';
-  }
+    document.documentElement.style.overflow = "auto";
+    document.body.scroll = "yes";
+  };
 
   return (
     <section id="services" className="services">
       {showModal && (
-          <Modal onClose={handleCloseModal}>
-            <ProductContent data={modalContent} />
-          </Modal>
+        <Modal onClose={handleCloseModal}>
+          <ProductContent data={modalContent} />
+        </Modal>
       )}
 
-
       <div className="container">
-
         <div className="section-title">
-          <h2>{t('products.title')}</h2>
-          <p>{t('products.desc')}</p>
+          <h2>{t("products.title")}</h2>
+          <p>{t("products.desc")}</p>
         </div>
 
         <div className="row">
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch c-p" onClick={() => onClick(1)}>
-            <div className="icon-box">
-              <div className="icon"><i className="fas fa-heartbeat"></i></div>
-              <h4><a href="">{t('products.premium1500')}</a></h4>
-              <p>{t('products.premium1500pr')}</p>
-            </div>
+          <div
+            className="col-lg-4 col-md-6 d-flex align-items-stretch c-p"
+            onClick={() => onClick(1)}
+          >
+            <ModelBlock
+              imgPath={require("../img/models/tractor-model1.png")}
+              hrefContent={t("products.premium1500")}
+              pContent={t("products.premium1500pr")}
+            />
           </div>
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 c-p" onClick={() => onClick(2)}>
-            <div className="icon-box">
-              <div className="icon"><i className="fas fa-pills"></i></div>
-              <h4><a href="">{t('products.advanced1200')}</a></h4>
-              <p>{t('products.advanced1200pr')}</p>
-            </div>
+          <div
+            className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 c-p"
+            onClick={() => onClick(2)}
+          >
+            <ModelBlock
+              imgPath={require("../img/models/tractor-model2.png")}
+              hrefContent={t("products.advanced1200")}
+              pContent={t("products.advanced1200pr")}
+            />
           </div>
 
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0 c-p" onClick={() => onClick(3)}>
-            <div className="icon-box">
-              <div className="icon"><i className="fas fa-hospital-user"></i></div>
-              <h4><a href="">{t('products.standard1000')}</a></h4>
-              <p>{t('products.standard1000pr')}</p>
-            </div>
+          <div
+            className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0 c-p"
+            onClick={() => onClick(3)}
+          >
+            <ModelBlock
+              imgPath={require("../img/models/tractor-model3.png")}
+              hrefContent={t("products.standard1000")}
+              pContent={t("products.standard1000pr")}
+            />
           </div>
 
           {/*commented by sam*/}
@@ -96,9 +104,8 @@ export default function Services(params) {
           {/*    <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>*/}
           {/*  </div>*/}
           {/*</div>*/}
-
         </div>
       </div>
     </section>
-  )
+  );
 }
