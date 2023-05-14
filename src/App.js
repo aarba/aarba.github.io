b384bcaa4ebe9fb686a10b9b3f405b1f4883f55d
samveltadevosyan <stadevosyan@servicetitan.com>
2023-05-14 16:59:12 +0400

    fix responsive nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { useEffect, useState } from 'react';
-import TopBar from './sections/topbar';
-import Header from './sections/header';
-import Hero from './sections/hero';
-import Services from './sections/services';
-import Footer from './sections/footer';
-import AboutUs from './sections/about-us';
-import Gallery from './sections/gallery';
-import './service/i18n';
-import './css/main.css'
-import './App.css';
-import { init } from './scripts'
-import Preloader from './sections/preloader';
-import { uiInit } from './scripts/main';
-import { Contacts } from './sections/contact';
-
-
+import { useEffect, useState } from "react";
+import TopBar from "./sections/topbar";
+import Header from "./sections/header";
+import Hero from "./sections/hero";
+import Services from "./sections/services";
+import Footer from "./sections/footer";
+import AboutUs from "./sections/about-us";
+import Gallery from "./sections/gallery";
+import "./service/i18n";
+import "./css/main.css";
+import "./App.css";
+import { init } from "./scripts";
+import Preloader from "./sections/preloader";
+import { uiInit } from "./scripts/main";
+import { Contacts } from "./sections/contact";
 
 function App() {
   const [stack, _] = useState([]);
@@ -26,26 +24,25 @@ function App() {
       init();
 
       stack.forEach((onLoad) => {
-        onLoad()
-      })
+        onLoad();
+      });
     }, 1000);
-  }, [])
-    console.log('heeelloo')
+  }, []);
   return (
     <div className="App">
-         <TopBar/>
-         <Header/>
-         <Hero />
-         <main id="main">
-          <AboutUs />
-          <section id="about" className="about"></section>
-          <section id="counts" className="counts"></section>
-          <Gallery/>
-          <Services/>
-          <Contacts />
-         </main>
-         <Preloader />
-         <Footer/>
+      <TopBar />
+      <Header />
+      <Hero />
+      <main id="main">
+        <AboutUs />
+        <section id="about" className="about"></section>
+        <section id="counts" className="counts"></section>
+        <Gallery />
+        <Services />
+        <Contacts />
+      </main>
+      <Preloader />
+      <Footer />
     </div>
   );
 }
